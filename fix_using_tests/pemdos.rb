18b1d590109fe4e097aefd7a91761f305b919732
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-29 22:57:01 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,10 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
+  count =0
   if string[0] == "s"
-    10 * "s" + string
+     "s" + string
+     count += 1
   else
     string
   end
